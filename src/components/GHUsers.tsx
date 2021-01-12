184037ce64df16e2e77faf5174086193d6166101
Yves Nunes Rodrigues Bodê <ynrbode@gmail.com>
2021-01-11 23:56:17 -0300

    removed unnecessary forward slash

--- src/components/GHUsers.tsx
+++ src/components/GHUsers.tsx
@@ -1,6 +1,6 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { AxiosError } from 'axios';
-import api from '../services/api';
+import React, { useCallback, useEffect, useState } from "react";
+import { AxiosError } from "axios";
+import github from "../services/github";
 
 interface User {
   id: number;
@@ -10,53 +10,50 @@ interface User {
 }
 
 const GHUsers = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
+  const [status, setStatus] = useState<'loading' | 'error' | 'success'>('loading');
   const [error, setError] = useState<AxiosError | null>(null);
+  const [data, setData] = useState<User[]>([]);
 
-  const fetchUsers = useCallback(async () => {
+  const fetchData = useCallback(async () => {
     try {
-      setIsError(false);
-      setIsLoading(true);
-      const response = await api.get<User[]>('users');
-      setIsLoading(false);
-      setUsers(response.data);
+      setStatus('loading')
+      const response = await github.get<User[]>('users');
+      setStatus('success');
+      setData(response.data);
     } catch (error) {
-      setIsError(true);
+      setStatus('error');
       setError(error);
     }
   }, []);
 
   useEffect(() => {
-    (async () => {
-      await fetchUsers();
-    })();
-  }, [fetchUsers]);
+    fetchData();
+  }, [fetchData]);
 
-  if (isError)
+  if (status === 'error')
     return <p>Error! {error && JSON.stringify(error.response?.data)}</p>;
 
-  if (isLoading) return <p>Loading...</p>;
+  if (status === 'loading') return <p>Loading...</p>;
 
   return (
     <div className="bg-gray-200 p-5 flex flex-wrap justify-center">
-      {users.map((user) => (
-        <div className="bg-gray-50 w-56 mx-3 my-3">
-          <img className="w-full" src={user.avatar_url} alt="User avatar" />
-          <div className="flex flex-col p-2">
-            <h2 className="font-semibold text-xl mb-5">{user.login}</h2>
-            <a
-              className="text-blue-600 self-end"
-              href={user.html_url}
-              target="_blank"
-              rel="noreferrer"
-            >
-              See on GitHub &gt;
-            </a>
+      {data &&
+        data.map((user) => (
+          <div className="bg-gray-50 w-56 mx-3 my-3">
+            <img className="w-full" src={user.avatar_url} alt="User avatar" />
+            <div className="flex flex-col p-2">
+              <h2 className="font-semibold text-xl mb-5">{user.login}</h2>
+              <a
+                className="text-blue-600 self-end"
+                href={user.html_url}
+                target="_blank"
+                rel="noreferrer"
+              >
+                See on GitHub &gt;
+              </a>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
